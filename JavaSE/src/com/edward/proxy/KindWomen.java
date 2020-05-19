078f4ae025154502039e3723aeced91de71ebd48
allen12425 <1242570922@qq.com>
2020-05-19 11:59:20 +0800

    代理模式

---
+++ JavaSE/src/com/edward/proxy/KindWomen.java
@@ -0,0 +1,19 @@
+package com.edward.proxy;
+
+public interface KindWomen {
+    /**
+     * 抛媚眼
+     */
+    public void makeEyesWithMen();
+
+
+    /**
+     *
+     */
+    public void playWithMen();
+
+
+
+
+
+}
---
+++ JavaSE/src/com/edward/proxy/XiMenQing.java
@@ -0,0 +1,9 @@
+package com.edward.proxy;
+
+public class XiMenQing {
+    public static void main(String[] args) {
+        WangPo wangPo = new WangPo();
+        wangPo.playWithMen();
+        wangPo.makeEyesWithMen();
+    }
+}
